ecd1d3f5af2e51964881a927af1c5b4ab87bba85
Suelarc <vovaroot@list.ru>
2026-09-01 00:10:38 +0300

    comm2

--- jss.js
+++ jss.js
@@ -2,13 +2,6 @@ const cards = document.querySelectorAll('.region-card');
 
 cards.forEach(card => {
     card.addEventListener('click', () => {
-
-        cards.forEach(otherCard => {
-            if (otherCard !== card) {
-                otherCard.classList.remove('scaled');
-            }
-        });
-
         card.classList.toggle('scaled');
     });
 });
